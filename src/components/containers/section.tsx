ff3f519f3471a50b37b26aba49904b90ab1eb3d7
Juan Sánchez <juanmsl.pk@gmail.com>
2026-06-14 13:45:09 -0500

    adding id

--- src/components/containers/section.tsx
+++ src/components/containers/section.tsx
@@ -5,11 +5,12 @@ import { cn } from '@helpers';
 interface SectionProps extends HTMLAttributes<HTMLElement> {
   compact?: boolean;
   contentClassName?: string;
+  id: string;
 }
 
-export function Section({ className, contentClassName, compact = false, children, ...props }: SectionProps) {
+export function Section({ id, className, contentClassName, compact = false, children, ...props }: SectionProps) {
   return (
-    <section className={cn(compact ? 'py-8 sm:py-10' : 'py-12 sm:py-16', className)} {...props}>
+    <section id={id} className={cn(compact ? 'py-8 sm:py-10' : 'py-12 sm:py-16', className)} {...props}>
       <div className={cn('mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8', contentClassName)}>{children}</div>
     </section>
   );
