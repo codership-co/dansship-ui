8512b7578b12d2e1e2b5f912a27612178648514d
Juan Sánchez <juanmsl.pk@gmail.com>
2026-07-02 19:00:23 -0500

    updating image sizes

--- src/components/modules/home/home-hero.tsx
+++ src/components/modules/home/home-hero.tsx
@@ -14,11 +14,11 @@ export const HomeHero = () => {
 
   return (
     <Section compact className='bg-gradient-hero'>
-      <div className='relative gap-10 xl:gap-20 home-hero transition-[all_300ms_ease] pb-7 pt-20 sm:pt-40 lg:pb-10'>
+      <div className='relative gap-10 xl:gap-20 home-hero transition-[all_300ms_ease] pb-7 pt-20 md:pt-30 lg:pt-40 lg:pb-10'>
         <div className='grid gap-4 content-center max-w-xl' style={{ gridArea: 'text' }}>
-          <Logotype className='h-8 hidden sm:inline-block' />
+          <Logotype className='h-8' />
 
-          <h1 className='text-accent leading-none m-0 mt-8'>
+          <h1 className='text-accent leading-none m-0 mt-6'>
             <Trans
               i18nKey='home:stitch.hero.kicker'
               components={{
@@ -44,11 +44,14 @@ export const HomeHero = () => {
           </div>
         </div>
 
-        <section className='relative size-60 lg:size-100 bg-accent rounded-full' style={{ gridArea: 'image' }}>
+        <section
+          className='relative size-40 sm:size-60 lg:size-100 bg-accent rounded-full'
+          style={{ gridArea: 'image' }}
+        >
           <img
             src='/assets/images/home/bailarina.png'
             alt='Dansship'
-            className='absolute left-1/2 top-1/2 max-w-[initial] w-7/8 -translate-x-1/2 -translate-y-[74%] block'
+            className='absolute left-1/2 top-1/2 max-w-[initial] w-7/8 -translate-x-1/2 -translate-y-[70%] block'
           />
         </section>
       </div>
