bc6169b90ee665501947dd6497431f22cfa668e1
Juan Manuel Sánchez Lozano <juanmsl.pk@gmail.com>
2026-07-17 18:26:00 -0500

    adding base structure for edit profile (#84)

--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -1,10 +1,19 @@
 import { Tabs } from 'polpo/components';
+import { cn } from 'polpo/helpers';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LuBookmark } from 'react-icons/lu';
 import { Link } from 'react-router';
 
 import { Container, Section } from '@components/containers';
-import { SavedFigures, AssignedSchedule, AvailabilityForm, ProfileHeader, ProfileAbout } from '@components/modules';
+import {
+  SavedFigures,
+  AssignedSchedule,
+  AvailabilityForm,
+  ProfileHeader,
+  ProfileAbout,
+  ProfileEdit,
+} from '@components/modules';
 import { FEATURE_FLAG, useAuth, SecurityGuard, useOrPermissions } from '@contexts';
 import { DansshipAPI } from '@core/api';
 import { PageURLS } from '@core/constants';
@@ -24,69 +33,76 @@ function ProfilePage() {
   const { response: savedFiguresResponse } = usePromise(() => DansshipAPI.figures.getSavedFigures());
   const hasInstructorPermissions = useOrPermissions([...InstructorPermissions.dashboard, PERMISSION.SCHEDULE_MANAGE]);
   const savedFigures = savedFiguresResponse?.data ?? [];
+  const [editMode, setEditMode] = useState(false);
 
   if (!user) {
     return <div className='text-center py-12'>{t('profile:notFound')}</div>;
   }
 
   return (
-    <main className='grid gap-8 md:gap-20'>
-      <ProfileHeader />
+    <main className='min-h-dvh grid gap-8 md:gap-20 content-start'>
+      <ProfileHeader editMode={editMode} onEdit={() => setEditMode(p => !p)} onChange={() => null} />
 
-      <Section footerMargin>
-        <Tabs defaultOpenTab={ProfileTabs.ABOUT}>
-          <Tabs.TabList
-            className='overflow-x-auto w-full grid-cols-1 sm:grid-cols-[1fr_1fr] grid-flow-dense md:grid-cols-none md:grid-flow-col'
-            color='primary'
-            variant='ghost'
-            tabs={[
-              { id: ProfileTabs.ABOUT, label: t('profile:profile') },
-              { id: ProfileTabs.FIGURES, label: t('profile:stats.savedFigures') },
-              ...(hasInstructorPermissions
-                ? [
-                    {
-                      id: ProfileTabs.SCHEDULE,
-                      label: t('instructor:dashboard.tabs.roster'),
-                    },
-                    { id: ProfileTabs.AVAILABILITY, label: t('instructor:dashboard.tabs.availability') },
-                  ]
-                : []),
-            ]}
-          />
+      {editMode ? (
+        <Section footerMargin>
+          <ProfileEdit />
+        </Section>
+      ) : (
+        <Section className={cn(editMode && 'hidden')} footerMargin>
+          <Tabs defaultOpenTab={ProfileTabs.ABOUT}>
+            <Tabs.TabList
+              className='overflow-x-auto w-full grid-cols-1 sm:grid-cols-[1fr_1fr] grid-flow-dense md:grid-cols-none md:grid-flow-col'
+              color='primary'
+              variant='ghost'
+              tabs={[
+                { id: ProfileTabs.ABOUT, label: t('profile:profile') },
+                { id: ProfileTabs.FIGURES, label: t('profile:stats.savedFigures') },
+                ...(hasInstructorPermissions
+                  ? [
+                      {
+                        id: ProfileTabs.SCHEDULE,
+                        label: t('instructor:dashboard.tabs.roster'),
+                      },
+                      { id: ProfileTabs.AVAILABILITY, label: t('instructor:dashboard.tabs.availability') },
+                    ]
+                  : []),
+              ]}
+            />
 
-          <section className='pt-8'>
-            <Tabs.TabPanel id={ProfileTabs.ABOUT}>
-              <ProfileAbout />
-            </Tabs.TabPanel>
+            <section className='pt-8'>
+              <Tabs.TabPanel id={ProfileTabs.ABOUT}>
+                <ProfileAbout />
+              </Tabs.TabPanel>
 
-            <Tabs.TabPanel id={ProfileTabs.FIGURES}>
-              <Container>
-                <section>
-                  <h4>{t('profile:stats.savedFigures')}</h4>
+              <Tabs.TabPanel id={ProfileTabs.FIGURES}>
+                <Container>
+                  <section>
+                    <h4>{t('profile:stats.savedFigures')}</h4>
 
-                  <Link
-                    to={PageURLS.figureSaved}
-                    className='flex items-center gap-2 font-medium text-primary hover:text-primary/90'
-                  >
-                    <LuBookmark className='h-4 w-4' />
-                    {t('common:viewDetails')}
-                  </Link>
-                </section>
+                    <Link
+                      to={PageURLS.figureSaved}
+                      className='flex items-center gap-2 font-medium text-primary hover:text-primary/90'
+                    >
+                      <LuBookmark className='h-4 w-4' />
+                      {t('common:viewDetails')}
+                    </Link>
+                  </section>
 
-                <SavedFigures figures={savedFigures} />
-              </Container>
-            </Tabs.TabPanel>
+                  <SavedFigures figures={savedFigures} />
+                </Container>
+              </Tabs.TabPanel>
 
-            <Tabs.TabPanel id={ProfileTabs.SCHEDULE}>
-              <AssignedSchedule />
-            </Tabs.TabPanel>
+              <Tabs.TabPanel id={ProfileTabs.SCHEDULE}>
+                <AssignedSchedule />
+              </Tabs.TabPanel>
 
-            <Tabs.TabPanel id={ProfileTabs.AVAILABILITY}>
-              <AvailabilityForm />
-            </Tabs.TabPanel>
-          </section>
-        </Tabs>
-      </Section>
+              <Tabs.TabPanel id={ProfileTabs.AVAILABILITY}>
+                <AvailabilityForm />
+              </Tabs.TabPanel>
+            </section>
+          </Tabs>
+        </Section>
+      )}
     </main>
   );
 }
