acd38ee204190e50562977fe2874132485a8a324
Juan Sánchez <juanmsl.pk@gmail.com>
2026-07-22 20:42:50 -0500

    adding small fixes

--- src/components/layouts/router-auth-layout.tsx
+++ src/components/layouts/router-auth-layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 
-import { SecurityGuard } from '@contexts';
+import { RootLoader } from '@components/loaders';
+import { SecurityGuard, useAuth } from '@contexts';
+import { useRouterLoading } from '@hooks';
 
 const SecurityOutlet = SecurityGuard(Outlet);
 
 export const RouterAuthLayout = () => {
+  const { ready } = useAuth();
+  const { location } = useNavigation();
+  const isRouterLoading = useRouterLoading();
+
   return (
     <section className='relative min-h-dvh grid grid-rows-[1fr]'>
-      <SecurityOutlet />
+      {(!ready || isRouterLoading) && !location?.pathname.startsWith('/auth') ? <RootLoader /> : <SecurityOutlet />}
     </section>
   );
 };
--- src/components/layouts/router-root-layout.tsx
+++ src/components/layouts/router-root-layout.tsx
@@ -1,23 +1,15 @@
-import { Outlet, useNavigation } from 'react-router';
+import { Outlet } from 'react-router';
 
-import { RootLoader } from '@components/loaders';
 import { Toaster } from '@components/ui';
-import { AuthProvider, FeatureFlagsProvider, SecurityGuard, useAuth } from '@contexts';
-import { useRouterLoading, useScrollToTop } from '@hooks';
+import { AuthProvider, FeatureFlagsProvider, SecurityGuard } from '@contexts';
+import { useScrollToTop } from '@hooks';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const { ready } = useAuth();
-  const { location } = useNavigation();
-  const isRouterLoading = useRouterLoading();
   useScrollToTop();
-
-  if ((!ready || isRouterLoading) && !location?.pathname.startsWith('/auth')) {
-    return <RootLoader />;
-  }
 
   return (
     <>
